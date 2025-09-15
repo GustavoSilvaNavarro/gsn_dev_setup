8c4cf1281d90e6959d051592a0eae40bc52b1a40
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-09-15 17:44:30 -0300

    🧱 Adding my Brewfile, for all the tools I use
    so this should list all the stuff and tools I use through hombrew

--- install.sh
+++ install.sh
@@ -8,7 +8,18 @@ install_homebrew() {
   if ! command -v brew &>/dev/null; then
     echo "🍺 Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    if ! command -v brew &>/dev/null; then
+      echo "❌ Error: Homebrew failed to install. Exiting."
+      exit 1  # Exit the script with an error code
+    else
+      brew update && brew upgrade && brew cleanup # update homebrew
+      clear
+      echo "✅ Homebrew successfully installed."
+    fi
   else
+    brew update && brew upgrade && brew cleanup
+    clear
     echo "✅ Homebrew already installed"
   fi
 }
